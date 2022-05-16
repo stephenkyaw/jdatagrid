75ddd4678bdd9f74bf1936775c4f8fcd3751af12
Kyaw Myo Aung <kyawmyoaung.dev@gmail.com>
2022-05-16 21:38:31 +0630

    re created repo

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
 # jdatagrid
-javascript data grid entry library
+javascript data grid entry lib
+
+**features**
+ - crud operation :heavy_check_mark:
+ - validation :x:
+ - sorting :x:
+ - searching :x:
+ - pagination :heavy_check_mark:
+ 
+:point_right: :link: [Demo Example](https://kyawmyoaung-dev.github.io/jgrid/) :grin:
+ 
+[![pages-build-deployment](https://github.com/kyawmyoaung-dev/jgrid/actions/workflows/pages/pages-build-deployment/badge.svg?branch=main)](https://github.com/kyawmyoaung-dev/jgrid/actions/workflows/pages/pages-build-deployment)
+
+**html**
+```
+<table id="jgrid" class="table"></table>
+```
+ **javascript**
+ ```
+document.addEventListener('DOMContentLoaded', function () {
+
+            let jgrid = document.querySelector('#jgrid').jgrid(
+                {
+                    item_types:
+                        [
+                            { name: 'name', header_text: 'Name', data_type: 'text', input_class_name: 'form-control' },
+                            { name: 'date_of_birth', header_text: 'DOB', data_type: 'date', input_class_name: 'form-control' },
+                            { name: 'email', header_text: 'Email', data_type: 'email', input_class_name: 'form-control' },
+                            { name: 'city', header_text: 'City', data_type: 'text', input_class_name: 'form-control', is_show: false }
+                        ],
+                    is_pagination: false,
+                    page_size: 3,
+                });
+ });
+ ```
+ **check log**
+```
+  btn_submit.addEventListener('click', function () {
+            console.log(jgrid);
+});
+```
+
+ **jgrid object log**
+
+```
+{
+    "item_types": [
+        {
+            "name": "name",
+            "header_text": "Name",
+            "data_type": "text",
+            "input_class_name": "form-control",
+            "is_show": true,
+            "is_sort": true,
+            "is_search": true
+        },
+        {
+            "name": "date_of_birth",
+            "header_text": "DOB",
+            "data_type": "date",
+            "input_class_name": "form-control",
+            "is_show": true,
+            "is_sort": true,
+            "is_search": true
+        },
+        {
+            "name": "email",
+            "header_text": "Email",
+            "data_type": "email",
+            "input_class_name": "form-control",
+            "is_show": true,
+            "is_sort": true,
+            "is_search": true
+        },
+        {
+            "name": "city",
+            "header_text": "City",
+            "data_type": "text",
+            "input_class_name": "form-control",
+            "is_show": false,
+            "is_sort": true,
+            "is_search": true
+        }
+    ],
+    "is_pagination": false,
+    "page_size": 3,
+    "table_instance": {},
+    "data": [
+        {
+            "name": "kma",
+            "date_of_birth": "2022-05-15",
+            "email": "kma@gmail.com",
+            "city": "yangon",
+            "auto_increment_id": 1
+        },
+        {
+            "name": "john",
+            "date_of_birth": "2022-05-16",
+            "email": "john@gmail.com",
+            "city": "mandalay",
+            "auto_increment_id": 2
+        },
+        {
+            "name": "may",
+            "date_of_birth": "2022-05-17",
+            "email": "may@gmail.com",
+            "city": "yangon",
+            "auto_increment_id": 3
+        }
+    ]
+}
+```
---
+++ js/jgrid.js
@@ -0,0 +1,287 @@
+"use strict";
+function jgrid(options) {
+
+    let default_options = { data: [], items: [], is_pagination: true, page_size: 5, current_page: 1 };
+
+    let { data, items, table, is_pagination, page_size, current_page } = { ...default_options, ...options };
+
+    let table_instance = document.querySelector(table);
+
+    this.get_items = function () {
+        return items;
+    }
+    this.get_data = function () {
+        return data;
+    }
+    this.get_table = function () {
+        return table;
+    }
+
+    function init() {
+        render_table_header();
+        render_table_input_controls();
+    }
+    function defined_item() {
+        let entity_item = Object.create(Object.prototype);
+        items.forEach(function (item) {
+            Object.defineProperty(entity_item, item.name, {
+                value: null,
+                writable: true,
+                enumerable: true,
+                configurable: true
+            });
+        });
+        entity_item.auto_increment_id = data.length + 1;
+        return entity_item;
+    }
+    function get_pagination_items() {
+        let _items = [];
+        if (is_pagination == true) {
+            if (data.length > page_size) {
+                _items = data.slice((current_page - 1) * page_size, current_page * page_size);
+            } else {
+                _items = data;
+            }
+        } else {
+            _items = data;
+        }
+        return _items;
+    }
+    function add_button_click_event() {
+        let entity_item = defined_item();
+        items.forEach(function (item) {
+            let input = table_instance.querySelector(`#${item.name}`);
+            entity_item[item.name] = input.value;
+        });
+        data.push(entity_item);
+        render_table_rows();
+    }
+    function edit_button_click_event(table_edit_index, auto_increment_id) {
+        let edit_button = table_instance.querySelector(`#item_edit_button_${auto_increment_id}`);
+        let delete_button = table_instance.querySelector(`#item_delete_button_${auto_increment_id}`);
+        let row = table_instance.rows[table_edit_index];
+        let edit_item = data[auto_increment_id - 1];//-1 for array index start by zero
+        if (edit_button.innerHTML == 'Edit') {
+            edit_button.innerHTML = 'Update';
+            delete_button.innerHTML = "Cancel";
+            items.forEach(function (item_type, _index) {
+                let clone_controls = table_instance.querySelector(`#${item_type.name}`).cloneNode(true);
+                clone_controls.id = `${item_type.name}_${auto_increment_id}`;
+                clone_controls.name = `${item_type.name}_${auto_increment_id}`;
+                clone_controls.value = edit_item[item_type.name];
+                row.cells[_index + 1].innerHTML = '';
+                row.cells[_index + 1].appendChild(clone_controls);
+            });
+        }
+        else if (edit_button.innerHTML == 'Update') {
+            items.forEach(function (item_type, _index) {
+                edit_item[item_type.name] = table_instance.querySelector(`#${item_type.name}_${auto_increment_id}`).value;
+            });
+            edit_button.innerHTML = "Edit";
+            delete_button.innerHTML = "Delete";
+            render_table_rows();
+        }
+    }
+    function delete_button_click_event(auto_increment_id) {
+        let delete_button = table_instance.querySelector(`#item_delete_button_${auto_increment_id}`);
+        if (delete_button.innerHTML == "Delete") {
+            data.splice((auto_increment_id - 1), 1);;
+            render_table_rows();
+        } else if (delete_button.innerHTML == "Cancel") {
+            render_table_rows();
+        }
+    }
+    function render_table_header() {
+        if (table_instance != null && table_instance != 'undefined') {
+            let row = table_instance.insertRow(0);
+            row.insertCell(0).innerHTML = "No.";
+            items.forEach(function (item, index) {
+                let cell = row.insertCell(index + 1);
+                cell.innerHTML = item.header_text;
+            });
+            //for add button cell
+            row.insertCell(items.length + 1);
+        }
+    }
+    function render_table_input_controls() {
+        if (table_instance != null && table_instance != 'undefined') {
+            let row = table_instance.insertRow(1);
+            //for No. cell
+            row.insertCell(0);
+            items.forEach(function (item, index) {
+                let cell = row.insertCell(index + 1);
+                let input = created_controls(item);
+                cell.appendChild(input);
+            });
+            //for add button cell
+            let add_btn_cell = row.insertCell(items.length + 1);
+            add_btn_cell.appendChild(created_add_button());
+        }
+    }
+    function clean_table_rows() {
+        let rowCount = table_instance.rows.length - 1;
+        while (rowCount >= 2) {
+            table_instance.deleteRow(rowCount);
+            rowCount--;
+        }
+    }
+    function serialize_auto_increment_id() {
+        data.forEach(function (item, index) {
+            item.auto_increment_id = index + 1;
+        });
+    }
+    function render_table_rows() {
+        clean_table_rows();
+        serialize_auto_increment_id();
+        let _items = get_pagination_items();
+        if (_items.length > 0) {
+            _items.forEach(function (item, index) {
+                let row = table_instance.insertRow(index + 2);
+                row.insertCell(0).innerHTML = item.auto_increment_id;
+                items.forEach(function (item_type, _index) {
+                    row.insertCell(_index + 1).innerHTML = item[item_type.name];
+                });
+                let edit_delete_cell = row.insertCell(items.length + 1);
+                let table_row_index = parseInt(index) + 2;
+                edit_delete_cell.appendChild(created_edit_button(table_row_index, item.auto_increment_id));
+                edit_delete_cell.appendChild(created_delete_button(item.auto_increment_id));
+            });
+
+            render_pagination();
+        }
+    }
+    function clean_pagination() {
+        let jgrid_pagination_container = table_instance.nextSibling;//for one or more table
+        if (jgrid_pagination_container != null && jgrid_pagination_container != 'undefined' && jgrid_pagination_container.className == "jgrid-pagination-container") {
+            while (jgrid_pagination_container.firstChild) {
+                jgrid_pagination_container.removeChild(jgrid_pagination_container.firstChild);
+            }
+            jgrid_pagination_container.remove();
+        }
+    }
+    function render_pagination() {
+        clean_pagination();
+        if (is_pagination == true) {
+            if (data.length > page_size) {
+                let jgrid_pagination_container = document.createElement('div');
+                jgrid_pagination_container.className = 'jgrid-pagination-container';
+                let jgrid_pagination = document.createElement('div');
+                jgrid_pagination.className = 'jgrid-pagination';
+                let total_page_number = Math.ceil(data.length / page_size);
+                for (let i = 1; total_page_number >= i; i++) {
+                    let jgrid_pagination_item = document.createElement('li');
+                    jgrid_pagination_item.className = 'jgrid-pagination-item';
+                    let jgird_pagination_item_link = document.createElement('a');
+                    jgird_pagination_item_link.className = 'jgird-pagination-item-link';
+                    jgird_pagination_item_link.innerHTML = i;
+                    jgird_pagination_item_link.href = '';
+                    jgird_pagination_item_link.onclick = function (e) {
+                        e.preventDefault();
+                        current_page = i;
+                        render_table_rows();
+                    }
+                    if (current_page == i) {
+                        jgird_pagination_item_link.className += " " + 'pagination-link-active';
+                    }
+                    jgrid_pagination_item.appendChild(jgird_pagination_item_link);
+
+                    jgrid_pagination.appendChild(jgrid_pagination_item);
+
+                }
+                jgrid_pagination_container.appendChild(jgrid_pagination);
+                table_instance.after(jgrid_pagination_container);
+            }
+        }
+    }
+    function created_add_button() {
+        let add_button = document.createElement('button');
+        add_button.innerHTML = 'Add';
+        add_button.className = 'btn btn-primary';
+        add_button.onclick = function () {
+            add_button_click_event();
+        }
+        return add_button;
+    }
+    function created_edit_button(table_row_index, auto_increment_id) {
+        let edit_button = document.createElement('button');
+        edit_button.className = 'btn btn-secondary me-2 btn_edit';
+        edit_button.innerHTML = 'Edit';
+        edit_button.id = `item_edit_button_${auto_increment_id}`;
+        edit_button.onclick = function () {
+            edit_button_click_event(table_row_index, auto_increment_id);
+        }
+        return edit_button;
+    }
+    function created_delete_button(auto_increment_id) {
+        let delete_button = document.createElement('button');
+        delete_button.className = 'btn btn-danger';
+        delete_button.innerHTML = 'Delete';
+        delete_button.id = `item_delete_button_${auto_increment_id}`;
+        delete_button.onclick = function () {
+            delete_button_click_event(auto_increment_id);
+        }
+        return delete_button;
+    }
+    function created_controls(item) {
+        let input;
+        switch (item.input_type) {
+            case 'text':
+                input = document.createElement('input');
+                input.type = 'text';
+                input.className = item.input_class_name;
+                break;
+            case 'date':
+                input = document.createElement('input');
+                input.type = 'date';
+                input.className = item.input_class_name;
+                break;
+            case 'email':
+                input = document.createElement('input');
+                input.type = 'email';
+                input.className = item.input_class_name;
+                break;
+            case 'select':
+                input = created_control_select(item);
+                break;
+            default:
+                input = document.createElement('input');
+                input.type = 'text';
+                input.className = item.input_class_name;
+        }
+        input.id = item.name;
+        input.name = item.name;
+        return input;
+    }
+    function created_control_select(item){
+        let select = document.createElement('select');
+        if(item.data.length > 0 ){
+            item.data.forEach(function(data){
+                let option = document.createElement('option');
+                option.value = data.value;
+                option.innerHTML = data.text;
+    
+                select.appendChild(option);
+            });
+            
+        }
+        select.className = item.input_class_name;
+        return select;
+    }
+
+    this.get_log = function () {
+        console.log('data log.....');
+        console.log(data);
+        console.log('items log.....');
+        console.log(items);
+        console.log('is_pagination log......');
+        console.log(is_pagination);
+        console.log('page_size log....');
+        console.log(page_size);
+        console.log('current_page log....');
+        console.log(current_page);
+    }
+
+
+    init();
+
+}
